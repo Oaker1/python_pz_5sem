de9a8974ea311124565071801a2986fa9ce7118a
Oaker1 <72872932+Oaker1@users.noreply.github.com>
2022-12-28 02:07:50 +0300

    Create web_client_test.py

---
+++ web_client_test.py
@@ -0,0 +1,8 @@
+import requests
+# r = requests.get('http://127.0.0.1:5000/users')
+# p = requests.post('http://127.0.0.1:5000/users', json={"name":"posttest", "street": "teststreet", "phone": "84948553"})
+# d = requests.delete('http://127.0.0.1:5000/users/133')
+# d = requests.delete('http://127.0.0.1:5000/users')
+# u = requests.put('http://127.0.0.1:5000/users/460', json={"name":"newname", "street": "teststreetupd", "phone": "8433"})
+
+#print(r.text)
